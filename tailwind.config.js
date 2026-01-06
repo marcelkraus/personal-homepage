490ccf636b3f6c60fefdaccf6c793766a1b853a4
Marcel Kraus <git@marcelkraus.de>
2026-01-06 11:01:41 +0100

    Refine colors to match 2026 CI idea

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,10 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                brand: '#607E4A', // Mix of all other brand colors (krausgebaut, krausgeborgt, krausgedruckt).
-
-                // Sky colors (Original Tailwind values)
-                'sky-2': '#7dd3fc', // Original: sky-300 (2x)
-                'sky-3': '#0284c7', // Original: sky-600 (14x)
-                'sky-5': '#0c4a6e', // Original: sky-900 (4x)
-
-                // Gray colors (Original Tailwind values)
-                'gray-1': '#d1d5db', // Original: gray-300 (11x)
-                'gray-3': '#111827', // Original: gray-900 (7x)
+                'brand-default': '#607E4A', // Mix of all other brand colors (krausgebaut, krausgeborgt, krausgedruckt)
+                'brand-highlight': '#879E77', // Brand + 25 %
+                'secondary-light': '#e5e7eb', // Original: gray-200
+                'secondary-dark': '#1f2937', // Original: gray-800
             }
         },
     },
